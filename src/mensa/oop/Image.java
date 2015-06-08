7dbfc98d3307aca8ef904f20f77acc2ad17eace5
Petar Vutov <github@pvutov.imap.cc>
2015-06-08 19:51:39 +0200

    image finished

--- src/mensa/oop/Image.java
+++ src/mensa/oop/Image.java
@@ -1,9 +1,16 @@
 package mensa.oop;
 
 public class Image {
-	// THESE FIELDS SOULD BE IMMUTABLE
+	// THESE FIELDS SHOULD BE IMMUTABLE
 	private int userid;
 	private String url;
+	private int hashCode;
+	
+	public Image(int userid, String url) {
+		this.userid = userid;
+		this.url = url;
+		hashCode = userid * 37 ^ url.hashCode();
+	}
 	
 	@Override
 	public boolean equals(Object other) {
@@ -19,5 +26,10 @@ public class Image {
 		}
 		
 		return false;
+	}
+	
+	@Override
+	public  int hashCode() {
+		return hashCode;
 	}
 }
