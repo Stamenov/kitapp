f4faaaf05256b824449daf6320e3ce178ff372ea
Petar Vutov <github@pvutov.imap.cc>
2015-07-14 08:25:34 +0200

    added: dynamic data from SW API

--- src/main/java/mensa/api/ApiRatingPoster.java
+++ src/main/java/mensa/api/ApiRatingPoster.java
@@ -3,7 +3,6 @@ package mensa.api;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -49,6 +48,7 @@ public class ApiRatingPoster {
 		private int value;
 		@JsonProperty("userid")
 		private int userid;
+		
 		public int getMealid() {
 			return mealid;
 		}
